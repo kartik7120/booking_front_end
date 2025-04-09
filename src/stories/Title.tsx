3b4cac9be78f54d40eb73e5412a41e3d0f253d1c
root <root@DESKTOP-DS82LMN>
2025-04-09 22:29:11 +0530

    Added some responsive classes for title component

--- src/stories/Title.tsx
+++ src/stories/Title.tsx
@@ -4,6 +4,8 @@ export interface TitleProps {
 
 export default function Title(props: TitleProps) {
     return (
-        <div className='text-6xl font-extrabold'>{props.title}</div>
+        <div className="w-full h-full text-8xl max-2xl:text-6xl font-extrabold max-xl:text-2xl max-lg:text-xl max-sm:text-base">
+            {props.title}
+        </div>
     )
 }
