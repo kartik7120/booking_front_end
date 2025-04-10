e5eddd9c34a533cee0fae010ace3e84d815f275a
root <root@DESKTOP-DS82LMN>
2025-04-10 21:06:34 +0530

    Added a model to open up a search box

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -2,6 +2,7 @@ import { IoTicketOutline } from "react-icons/io5";
 import { FaSearch } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import './header.css';
+import { Link } from "react-router";
 
 type User = {
   name: string;
@@ -17,19 +18,37 @@ export interface HeaderProps {
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps) => (
   <header>
     <div className="storybook-header">
-      <div className="flex items-center">
+      {/* <Link to="/"> */}
+      <div className="flex items-center btn btn-ghost">
         <IoTicketOutline size={30} color="#FEE505" />
         <h1 className='antialiased'>GoWatch</h1>
       </div>
+      {/* </Link> */}
       <div>
         <div className="flex flex-row items-center">
           <button className="btn btn-circle">
-            <FaSearch />
-          </button>
-          <button className="btn">
-            <p className="text-lg">
-              Home
-            </p>
+            <button className="btn" onClick={() => {
+              const dialog = document.getElementById("my_modal_2") as HTMLDialogElement;
+              if (dialog) {
+                dialog.showModal();
+              }
+            }}>
+              <FaSearch />
+            </button>
+            <dialog id="my_modal_2" className="modal">
+              <div className="modal-box w-fit">
+                <div className="flex flex-col items-center gap-y-3">
+                  <h2 className="text-3xl">Search your movie or event</h2>
+                  <label className="input">
+                    <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g></svg>
+                    <input type="search" className="grow" placeholder="Search" />
+                  </label>
+                </div>
+              </div>
+              <form method="dialog" className="modal-backdrop">
+                <button>close</button>
+              </form>
+            </dialog>
           </button>
           <button className="btn">
             <p className="text-lg">
