52c4f421d1e883a3af3f9b8898992e54b218537b
root <root@DESKTOP-DS82LMN>
2025-05-29 21:20:14 +0530

    Added a curved theatre screen

---
+++ src/components/MovieTimeSlots/CurvedTheatreScreen.tsx
@@ -0,0 +1,25 @@
+export default function CurvedTheatreScreen() {
+    return (
+        <svg
+            width="100%"
+            height="100%"
+            viewBox="0 0 200 100"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                right: 0,
+                zIndex: 1,
+            }}
+
+        >
+            <path
+                d="M 46 68 Q 100 0 159 67"
+                fill="none"
+                stroke="#fff"
+                strokeWidth="1"
+            />
+        </svg>
+    )
+}
---
+++ src/stories/CurvedTheatreScreen.stories.ts
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from "@storybook/react";
+import CurvedTheatreScreen from "../components/MovieTimeSlots/CurvedTheatreScreen";
+
+const meta = {
+    title: "Seat Selection/Curved Theatre Screen",
+    component: CurvedTheatreScreen,
+} satisfies Meta<typeof CurvedTheatreScreen>;
+
+export default meta;
+
+type Story = StoryObj<typeof CurvedTheatreScreen>;
+
+export const Primary: Story = {
+
+}
